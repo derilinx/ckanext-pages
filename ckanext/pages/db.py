468ab9ca3bb46c9f51314f838c0dc771350ca6be
Jenom Kassim <jenomkassim@derilinx.com>
2024-07-17 02:35:55 +0000

    Added UI to add new language variation of a page

--- ckanext/pages/db.py
+++ ckanext/pages/db.py
@@ -57,6 +57,26 @@ class Page(DomainObject):
             query = query.order_by(cls.created.desc())
         return query.all()
 
+    def set_title(self, title, lang='default'):
+        '''Sets the title for the specified language.'''
+        if not self.title:
+            self.title = {}
+        self.title[lang] = title
+
+    def get_title(self, lang='default'):
+        '''Gets the title for the specified language.'''
+        return self.title.get(lang, self.title.get('default', ''))
+
+    def set_content(self, content, lang='default'):
+        '''Sets the content for the specified language.'''
+        if not self.content:
+            self.content = {}
+        self.content[lang] = content
+
+    def get_content(self, lang='default'):
+        '''Gets the content for the specified language.'''
+        return self.content.get(lang, self.content.get('default', ''))
+
 
 def define_tables():
     sql_upgrade_03 = ("ALTER TABLE ckanext_pages add column featured boolean DEFAULT 'f';",
