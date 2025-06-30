88bb0066c0ae9d1bc5cea02792aab635c9c2a071
Ben Soroos <bensoroos@derilinx.com>
2025-06-30 14:36:29 +0100

    ckeditor plugins: extract to support multiple ckeditors on a page

--- ckanext/pages/assets/js/ckedit.js
+++ ckanext/pages/assets/js/ckedit.js
@@ -1,3 +1,173 @@
+CKEDITOR.dialog.add('embedResourceView', function (editor) {
+  var packages = {};
+  var ckan_client = editor.config.ckan_client;
+  return {
+    title: 'Embed Resource View',
+    contents: [
+      {
+        id: 'resource-view-tab',
+        label: 'View',
+        elements: [
+          {
+            type: 'select',
+            id: 'package',
+            label: 'Dataset',
+            items: [],
+            setup: function() {
+              var select = this;
+              ckan_client.call('GET', 'package_search', '', function onSucess(result) {
+                result.result.results.forEach(function (pkg) {
+                  select.add(pkg.title, pkg.id);
+                  packages[pkg.id] = pkg;
+                });
+                if (result.result.results.length > 0) {
+                  select.setValue(result.result.results[0].id);
+                }
+              }, function onError(error) {
+                console.error(error);
+                select.add('An error occurred fetching datasets', 'error');
+              });
+            },
+            onChange: function() {
+              var select = this.getDialog().getContentElement('resource-view-tab', 'resource');
+              select.clear();
+              packages[this.getValue()].resources.forEach(function (resource) {
+                select.add(resource.name, resource.id);
+              });
+              if (packages[this.getValue()].resources.length > 0) {
+                select.setValue(packages[this.getValue()].resources[0].id);
+              }
+              select.enable();
+            }
+          },
+          {
+            type: 'select',
+            id: 'resource',
+            label: 'Resource',
+            items: [],
+            setup: function() {
+              this.disable();
+            },
+            onChange: function() {
+              var select = this.getDialog().getContentElement('resource-view-tab', 'view');
+              select.clear();
+              ckan_client.call('GET', 'resource_view_list', '?id=' + this.getValue(), function onSuccess(result) {
+                result.result.forEach(function (view) {
+                  select.add(view.title, view.id);
+                  select.enable();
+                });
+              }, function onError(err) {
+                  console.error(err);
+                  select.add('An error occurred fetching resource views', 'error');
+                  select.enable();
+              });
+            }
+          },
+          {
+            type: 'select',
+            id: 'view',
+            label: 'View',
+            items: [],
+            setup: function() {
+              this.disable();
+            },
+          },
+        ],
+      },
+      {
+        id: 'display-tab',
+        label: 'Display',
+        elements: [
+          {
+            type: 'text',
+            id: 'width',
+            label: 'Width',
+            default: '100%',
+          },
+          {
+            type: 'text',
+            id: 'height',
+            label: 'height',
+            default: '50vh',
+          },
+        ],
+      },
+    ],
+    onShow: function () {
+      this.setupContent();
+    },
+    onOk: function() {
+      var el = editor.document.createElement('iframe');
+      var pkg = this.getValueOf('resource-view-tab', 'package');
+      var resource = this.getValueOf('resource-view-tab', 'resource');
+      var view = this.getValueOf('resource-view-tab', 'view');
+      el.setAttribute('src', site_url + '/dataset/' + pkg + '/resource/' + resource + '/view/' + view);
+      el.setAttribute('style', "width: " + this.getValueOf('display-tab', 'width') + '; height: ' + this.getValueOf('display-tab', 'height') + ';');
+      editor.insertElement(el);
+    },
+  }
+});
+
+CKEDITOR.plugins.add('resourceviewembed', {
+  init: function (editor) {
+    editor.addCommand('embedResourceView', new CKEDITOR.dialogCommand('embedResourceView', {allowedContent: 'iframe[src,style]'}));
+    editor.ui.addButton('resourceview', {
+      label: 'Embed Resource View',
+      command: 'embedResourceView',
+      toolbar: 'insert',
+      icon: 'source',
+    });
+  },
+});
+
+CKEDITOR.dialog.add('embedDashboard', function (editor) {
+  return {
+    title: 'Embed Superset Dashboard',
+    contents: [{
+      id: 'dashboard-tab',
+      elements: [
+        {
+          type: 'select',
+          id: 'dashboard',
+          label: 'The dashboard to embed',
+          items: editor.config.supersetDashboards.map(function (dashboard) {
+            return [dashboard.title, dashboard.url];
+          }),
+        },
+        {
+          type: 'text',
+          id: 'dashboard-width',
+          label: 'Embed width',
+          default: '100%',
+        },
+        {
+          type: 'text',
+          id: 'dashboard-height',
+          label: 'Embed height',
+          default: '65vh',
+        },
+      ],
+    }],
+    onOk: function() {
+      var el = editor.document.createElement('iframe');
+      el.setAttribute('src', this.getValueOf('dashboard-tab', 'dashboard'));
+      el.setAttribute('style', "width: " + this.getValueOf('dashboard-tab', 'dashboard-width') + '; height: ' + this.getValueOf('dashboard-tab', 'dashboard-height') + ';');
+      editor.insertElement(el);
+    },
+  };
+});
+CKEDITOR.plugins.add('supersetdashboards', {
+  init: function (editor) {
+    editor.addCommand('embedDashboard', new CKEDITOR.dialogCommand('embedDashboard', {allowedContent: 'iframe[src,style]'}));
+    editor.ui.addButton('Dashboard', {
+      label: 'Embed Superset Dashboard',
+      command: 'embedDashboard',
+      toolbar: 'insert',
+      icon: 'source',
+    });
+  },
+});
+
 this.ckan.module("ckedit", function(jQuery, _) {
     return {
         options: {
@@ -52,177 +222,12 @@ this.ckan.module("ckedit", function(jQuery, _) {
       config.filebrowserUploadUrl = site_url + 'pages_upload';
 
       var ckan_client = this.sandbox.client;
-      var packages = {};
-      CKEDITOR.dialog.add('embedResourceView', function (editor) {
-        return {
-          title: 'Embed Resource View',
-          contents: [
-            {
-              id: 'resource-view-tab',
-              label: 'View',
-              elements: [
-                {
-                  type: 'select',
-                  id: 'package',
-                  label: 'Dataset',
-                  items: [],
-                  setup: function() {
-                    var select = this;
-                    ckan_client.call('GET', 'package_search', '', function onSucess(result) {
-                      result.result.results.forEach(function (pkg) {
-                        select.add(pkg.title, pkg.id);
-                        packages[pkg.id] = pkg;
-                      });
-                      if (result.result.results.length > 0) {
-                        select.setValue(result.result.results[0].id);
-                      }
-                    }, function onError(error) {
-                      console.error(error);
-                      select.add('An error occurred fetching datasets', 'error');
-                    });
-                  },
-                  onChange: function() {
-                    var select = this.getDialog().getContentElement('resource-view-tab', 'resource');
-                    select.clear();
-                    packages[this.getValue()].resources.forEach(function (resource) {
-                      select.add(resource.name, resource.id);
-                    });
-                    if (packages[this.getValue()].resources.length > 0) {
-                      select.setValue(packages[this.getValue()].resources[0].id);
-                    }
-                    select.enable();
-                  }
-                },
-                {
-                  type: 'select',
-                  id: 'resource',
-                  label: 'Resource',
-                  items: [],
-                  setup: function() {
-                    this.disable();
-                  },
-                  onChange: function() {
-                    var select = this.getDialog().getContentElement('resource-view-tab', 'view');
-                    select.clear();
-                    ckan_client.call('GET', 'resource_view_list', '?id=' + this.getValue(), function onSuccess(result) {
-                      result.result.forEach(function (view) {
-                        select.add(view.title, view.id);
-                        select.enable();
-                      });
-                    }, function onError(err) {
-                        console.error(err);
-                        select.add('An error occurred fetching resource views', 'error');
-                        select.enable();
-                    });
-                  }
-                },
-                {
-                  type: 'select',
-                  id: 'view',
-                  label: 'View',
-                  items: [],
-                  setup: function() {
-                    this.disable();
-                  },
-                },
-              ],
-            },
-            {
-              id: 'display-tab',
-              label: 'Display',
-              elements: [
-                {
-                  type: 'text',
-                  id: 'width',
-                  label: 'Width',
-                  default: '100%',
-                },
-                {
-                  type: 'text',
-                  id: 'height',
-                  label: 'height',
-                  default: '50vh',
-                },
-              ],
-            },
-          ],
-          onShow: function () {
-            this.setupContent();
-          },
-          onOk: function() {
-            var el = editor.document.createElement('iframe');
-            var pkg = this.getValueOf('resource-view-tab', 'package');
-            var resource = this.getValueOf('resource-view-tab', 'resource');
-            var view = this.getValueOf('resource-view-tab', 'view');
-            el.setAttribute('src', site_url + '/dataset/' + pkg + '/resource/' + resource + '/view/' + view);
-            el.setAttribute('style', "width: " + this.getValueOf('display-tab', 'width') + '; height: ' + this.getValueOf('display-tab', 'height') + ';');
-            editor.insertElement(el);
-          },
-        }
-      });
-
-      CKEDITOR.plugins.add('resourceviewembed', {
-        init: function (editor) {
-          editor.addCommand('embedResourceView', new CKEDITOR.dialogCommand('embedResourceView', {allowedContent: 'iframe[src,style]'}));
-          editor.ui.addButton('resourceview', {
-            label: 'Embed Resource View',
-            command: 'embedResourceView',
-            toolbar: 'insert',
-            icon: 'source',
-          });
-        },
-      });
+      config.ckan_client = ckan_client;
       config.extraPlugins += ',resourceviewembed';
 
       var supersetDashboards = this.options.superset_dashboards;
       if (supersetDashboards.length > 0) {
-        CKEDITOR.dialog.add('embedDashboard', function (editor) {
-          return {
-            title: 'Embed Superset Dashboard',
-            contents: [{
-              id: 'dashboard-tab',
-              elements: [
-                {
-                  type: 'select',
-                  id: 'dashboard',
-                  label: 'The dashboard to embed',
-                  items: supersetDashboards.map(function (dashboard) {
-                    return [dashboard.title, dashboard.url];
-                  }),
-                },
-                {
-                  type: 'text',
-                  id: 'dashboard-width',
-                  label: 'Embed width',
-                  default: '100%',
-                },
-                {
-                  type: 'text',
-                  id: 'dashboard-height',
-                  label: 'Embed height',
-                  default: '65vh',
-                },
-              ],
-            }],
-            onOk: function() {
-              var el = editor.document.createElement('iframe');
-              el.setAttribute('src', this.getValueOf('dashboard-tab', 'dashboard'));
-              el.setAttribute('style', "width: " + this.getValueOf('dashboard-tab', 'dashboard-width') + '; height: ' + this.getValueOf('dashboard-tab', 'dashboard-height') + ';');
-              editor.insertElement(el);
-            },
-          };
-        });
-        CKEDITOR.plugins.add('supersetdashboards', {
-          init: function (editor) {
-            editor.addCommand('embedDashboard', new CKEDITOR.dialogCommand('embedDashboard', {allowedContent: 'iframe[src,style]'}));
-            editor.ui.addButton('Dashboard', {
-              label: 'Embed Superset Dashboard',
-              command: 'embedDashboard',
-              toolbar: 'insert',
-              icon: 'source',
-            });
-          },
-        });
+        config.supersetDashboards = supersetDashboards;        
         config.extraPlugins += ',supersetdashboards';
       }
       // Override default config options with ones provided by plugins
